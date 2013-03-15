9459982010f79160bd42dcd1f53e45b0af457ebe
liushuaikobe <liushuaikobe@gmail.com>
2013-03-15 12:58:38 +0800

    Add the answer of riddle 10

--- 10/pc_10.py
+++ 10/pc_10.py
@@ -1,20 +1,26 @@
 def main():
+	# Look-and-say sequence
+	# http://en.wikipedia.org/wiki/Look-and-say_sequence
 	seq = '1'
 	current_num = ''
 	counter = 0
 	a = seq[0]
-	for i in xrange(5):
+	# print seq
+	for i in xrange(30):
 		for c in seq:
 			if c == a:
 				counter += 1
 			else:
 				current_num = ''.join((current_num, str(counter),a))
+				a = c
 				counter = 1
 		current_num = ''.join((current_num, str(counter),a))
-		counter = 1
-		print current_num
+		# print current_num
 		seq = current_num
+		counter = 0
 		current_num = ''
+		a = seq[0]
+	print len(seq)
 
 if __name__ == '__main__':
 	main()
